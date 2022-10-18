0d74c144b408ddef47608d380141dc4c69e40b21
Jacøb <85955074+cuber1515@users.noreply.github.com>
2022-10-17 18:39:26 -0700

    Added arm, sub claw, claw
    * Arm:
      - hold `b` to raise claw
      - hold `y` to raise arm
    
    * Sub-claw:
      - When the sensor `subClawS` sees blue it will close and go back for a bit and then open to life the cone up right
    
    * Claw:
      - Press `a` to close it
      - Press `a` to open it

--- teleOpCode.java
+++ teleOpCode.java
@@ -6,42 +6,53 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.ColorSensor;
 
 @TeleOp
 
 public class teleOpCode extends LinearOpMode {
-    DcMotor FR, FL, BR, BL;
-    Servo LS, RS;
+    DcMotor FR, FL, BR, BL, AL, LA;
+    Servo LS, RS, CS;
+    ColorSensor subClawS;
 
     @Override
     public void runOpMode() throws InterruptedException {
+        // Motors and servos
         FR = hardwareMap.dcMotor.get("Front Right");
         FL = hardwareMap.dcMotor.get("Front Left");
         BR = hardwareMap.dcMotor.get("Back Right");
         BL = hardwareMap.dcMotor.get("Back Left");
+        AL = hardwareMap.dcMotor.get("Arm Lift");
+        LA = hardwareMap.dcMotor.get("Linear Actuator");
 
         LS = hardwareMap.servo.get("Left Servo");
         RS = hardwareMap.servo.get("Right Servo");
+        CS = hardwareMap.servo.get("Claw Servo");
 
         FR.setDirection((DcMotor.Direction.REVERSE));
 
+        // Sensors
+        subClawS = hardwareMap.colorSensor.get("Sub Claw Sensor");
+
+        telemetry.addData(">", "Press Play to start op mode");
+        telemetry.update();
         waitForStart();
 
         if (opModeIsActive()) {
             while (opModeIsActive()) {
 
                 // MOVE ROBOT
-                FR.setPower(gamepad1.right_stick_y * 0.5);
-                BR.setPower(gamepad1.right_stick_y * 0.5);
-                FL.setPower(gamepad1.left_stick_y * 0.5);
-                BL.setPower(gamepad1.left_stick_y * 0.5);
+                FR.setPower(gamepad1.right_stick_y * .25);
+                BR.setPower(gamepad1.right_stick_y * .25);
+                FL.setPower(gamepad1.left_stick_y * .25);
+                BL.setPower(gamepad1.left_stick_y * .25);
 
                 // Move to the left if the left trigger is pressed
                 if (gamepad1.left_trigger > 0) {
-                    FR.setPower(gamepad1.left_trigger * .5);
-                    BL.setPower(gamepad1.left_trigger * .5);
-                    FL.setPower(-gamepad1.left_trigger * .5);
-                    BR.setPower(-gamepad1.left_trigger * .5);
+                    FR.setPower(gamepad1.left_trigger * .25);
+                    BL.setPower(gamepad1.left_trigger * .25);
+                    FL.setPower(-gamepad1.left_trigger * .25);
+                    BR.setPower(-gamepad1.left_trigger * .25);
 
                     if (gamepad1.left_trigger == 0) {
                         FR.setPower(0);
@@ -53,10 +64,10 @@ public class teleOpCode extends LinearOpMode {
 
                 // Move to the right if the right trigger is pressed
                 if (gamepad1.right_trigger > 0) {
-                    FR.setPower(-gamepad1.right_trigger * .5);
-                    BL.setPower(-gamepad1.right_trigger * .5);
-                    FL.setPower(gamepad1.right_trigger * .5);
-                    BR.setPower(gamepad1.right_trigger * .5);
+                    FR.setPower(-gamepad1.right_trigger * .25);
+                    BL.setPower(-gamepad1.right_trigger * .25);
+                    FL.setPower(gamepad1.right_trigger * .25);
+                    BR.setPower(gamepad1.right_trigger * .25);
 
                     if (gamepad1.right_trigger == 0) {
                         FR.setPower(0);
@@ -65,9 +76,53 @@ public class teleOpCode extends LinearOpMode {
                         BL.setPower(0);
                     }
                 }
+
+                // Sub claw stuff on blue
+                if (subClawS.blue() > 20) {
+                    LS.setPosition(45);
+                    RS.setPosition(45);
+
+                    sleep(100);
+
+                    FR.setPower(0.25);
+                    FL.setPower(0.25);
+                    BR.setPower(0.25);
+                    BL.setPower(0.25);
+
+                    sleep(500);
+
+                    FR.setPower(0);
+                    FL.setPower(0);
+                    BR.setPower(0);
+                    BL.setPower(0);
+
+                    LS.setPosition(40);
+                    RS.setPosition(40);
+                }
+
+                // Close/Open claw
                 if (gamepad1.a) {
-                    LS.setPosition(0.45);
-                    RS.setPosition(0.50);
+                    if (CS.getPosition() == 50){
+                        CS.setPosition(55);
+                    } else {
+                        CS.setPosition(50);
+                    }
+                }
+
+                // Raising claw
+                if (gamepad1.b) {
+                    AL.setPower(0.10);
+                    if (!gamepad1.b) {
+                        AL.setPower(0);
+                    }
+                }
+
+                // Raising arm
+                if (gamepad1.y) {
+                    LA.setPower(0.25);
+                    if (!gamepad1.y) {
+                        LA.setPower(0);
+                    }
                 }
             }
         }
