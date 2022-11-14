73e205e810802ebb1388cb7ececa81053f2e52fd
Jacøb <85955074+cuber1515@users.noreply.github.com>
2022-11-14 15:49:13 -0800

    Fresh new code
    * Goes forward 24 inches and then lowers the arm.

--- autonomousCode.java
+++ autonomousCode.java
@@ -7,40 +7,9 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 
-import java.util.List;
-import org.firstinspires.ftc.robotcore.external.ClassFactory;
-import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
-import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
-import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector;
-import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
-
-import org.firstinspires.ftc.teamcode.methodForEncoders;
-
-
 @Autonomous
 @Disabled
 public class autonomousCode extends LinearOpMode{
-
-    /**
-     * setting up TensorFlow
-     */
-
-    private static final String TFOD_MODEL_ASSET = "unknown";
-    private static final String[] LABELS = {
-            "unkown"
-    };
-
-    private static final String VUFORIA_KEY =
-            " -- YOUR NEW VUFORIA KEY GOES HERE  --- ";
-
-    private VuforiaLocalizer vuforia;
-
-    private TFObjectDetector tfod;
-
-
-    /**
-     * end of TensorFlow
-     */
 
     // start encoders
     public void startEncoders() {
@@ -66,151 +35,52 @@ public class autonomousCode extends LinearOpMode{
         BR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
     }
 
+    // start encoders arm
+    public void startEncodersA() {
+        AM.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        LAM.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
 
-    /**
-     *Go forward
-     *
-     * example: goForward(1000, 0.95) will go forward at 95% power for 1 second
-     * example: goForward(0, 0.95) will go forward at 95% power indefinitely
-     */
-    public void goForward(int tIme, int power) {
+    // exit encoders arm
+    public void exitEncodersA() {
+        AM.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        LAM.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+    }
+
+    // restart encoders arm
+    public void resetEncodersA() {
+        AM.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        LAM.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+    }
+
+    // drive with encoders
+    public void driveEncoders(double power, int target) {
+        FR.setTargetPosition(target);
+        FL.setTargetPosition(target);
+        BR.setTargetPosition(target);
+        BL.setTargetPosition(target);
+
         FR.setPower(power);
         FL.setPower(power);
         BR.setPower(power);
         BL.setPower(power);
 
-        if (tIme != 0) { // IF THIS IS SET TO ANY NUMBER OTHER THAN 0 IT WILL MAKE IT SO THAT IT GOES
-            sleep(tIme); // FORWARD FOR HOWEVER LONG IT IS SET TO, IF IT'S 0 IT WILL JUST SET
-                         // THE MOTORS TO THE POWER INDEFINITELY
-            FR.setPower(0);
-            FL.setPower(0);
-            BR.setPower(0);
-            BL.setPower(0);
-        }
-    }
-
-    /**
-     *Go backward
-     *
-     * example: goBackward(1000, 0.95) will go backward at 95% power for 1 second
-     * example: goBackward(0, 0.95) will go backward at 95% power indefinitely
-     */
-    public void goBackward(int tIme, double power) {
-        FR.setPower(-power);
-        FL.setPower(-power);
-        BR.setPower(-power);
-        BL.setPower(-power);
-
-        if (tIme != 0) { // IF THIS IS SET TO ANY NUMBER OTHER THAN 0 IT WILL MAKE IT SO THAT IT GOES
-            sleep(tIme); // BACKWARD FOR HOWEVER LONG IT IS SET TO, IF IT'S 0 IT WILL JUST SET
-                         // THE MOTORS TO THE POWER INDEFINITELY
-            FR.setPower(0);
-            FL.setPower(0);
-            BR.setPower(0);
-            BL.setPower(0);
-        }
-    }
-
-    /**
-     * turning
-     *
-     * example: turn(2000, "right"); will turn right at 50% power for 2 seconds
-     */
-    public void turn(int tIme, String direction) {
-        if (direction == "right") {
-            FL.setPower(0.5);
-            BL.setPower(0.5);
-            FR.setPower(-0.5);
-            BR.setPower(-0.5);
-        } else if (direction == "left") {
-            FR.setPower(0.5);
-            BR.setPower(0.5);
-            FL.setPower(-0.5);
-            BL.setPower(-0.5);
-        }
-
-        if (tIme != 0) { // IF THIS IS SET TO ANY NUMBER OTHER THAN 0 IT WILL MAKE IT SO THAT IT
-            sleep(tIme); // TURNS FOR HOWEVER LONG IT IS SET TO, IF IT'S 0 IT WILL JUST SET
-                         // THE MOTORS TO THE POWER INDEFINITELY
-            FR.setPower(0);
-            FL.setPower(0);
-            BR.setPower(0);
-            BL.setPower(0);
-        }
-    }
-
-    /**
-     * slide right/left
-     *
-     * example: slide(1500, left); will slide to the left at 50% power for 1.5 seconds
-     */
-    public void slide(int tIme, String direction) {
-        if (direction == "right") {
-            FL.setPower(0.5);
-            BR.setPower(0.5);
-            FR.setPower(-0.5);
-            BL.setPower(-0.5);
-        } else if (direction == "left") {
-            FR.setPower(0.5);
-            BL.setPower(0.5);
-            FL.setPower(-0.5);
-            BR.setPower(-0.5);
-        }
-
-        if (tIme != 0) { // IF THIS IS SET TO ANY NUMBER OTHER THAN 0 IT WILL MAKE IT SO THAT IT
-            sleep(tIme); // TURNS FOR HOWEVER LONG IT IS SET TO, IF IT'S 0 IT WILL JUST SET
-                         // THE MOTORS TO THE POWER INDEFINITELY
-            FR.setPower(0);
-            FL.setPower(0);
-            BR.setPower(0);
-            BL.setPower(0);
-        }
-    }
-
-    /**
-     * Methods for encoders
-     *
-     * example for distance:
-     *  double target = encodersTarget.calculateToPlaceDistance(5);
-     *  encoders(target);
-     * will go forward 5 inches at power 25%
-     *
-     * example for rotations:
-     *  double target = encodersTarget.calculateToPlaceRotations(1.5);
-     *  encoders(target);
-     * will go forward 1.5 rotations
-     */
-    methodForEncoders encodersTarget = new methodForEncoders();
-    public void encoders(int targetToPlace) {
-
-        startEncoders();
-
-        FL.setTargetPosition(targetToPlace);
-        FR.setTargetPosition(targetToPlace);
-        BL.setTargetPosition(targetToPlace);
-        BR.setTargetPosition(targetToPlace);
-
-        FL.setPower(0.25);
-        FR.setPower(0.25);
-        BL.setPower(0.25);
-        BR.setPower(0.25);
-
-        FL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         FR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        BL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        FL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         BR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        BL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
-        while (FR.isBusy() || FL.isBusy() || BR.isBusy() || BL.isBusy()) {
+        while (FR.isBusy() || FL.isBusy()) {
         }
 
-        FL.setPower(0);
         FR.setPower(0);
-        BL.setPower(0);
+        FL.setPower(0);
         BR.setPower(0);
+        BL.setPower(0);
     }
 
-    DcMotor FR, FL, BR, BL;
-    Servo LS, RS;
+    DcMotor FR, FL, BR, BL, AM, LAM;
+    Servo CS;
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -218,43 +88,34 @@ public class autonomousCode extends LinearOpMode{
         FL = hardwareMap.dcMotor.get("Front Left");
         BR = hardwareMap.dcMotor.get("Back Right");
         BL = hardwareMap.dcMotor.get("Back Left");
+        AM = hardwareMap.dcMotor.get("Arm Lift");
+        LAM = hardwareMap.dcMotor.get("Linear Actuator");
 
-        LS = hardwareMap.servo.get("Left Servo");
-        RS = hardwareMap.servo.get("Right Servo");
+        AM.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        LAM.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
-        FR.setDirection((DcMotor.Direction.REVERSE));
+        CS = hardwareMap.servo.get("Claw Servo");
+
+        waitForStart();
+
+        resetEncoders();startEncoders();
+        double circumference = 3.14 * 4;
+        double rotationsNeeded = 24 / circumference;
+        int encoderDrivingTarget = (int) (rotationsNeeded * 537.7);
+
+        driveEncoders(0.5, encoderDrivingTarget);
+        exitEncoders();
+
+        resetEncodersA();startEncodersA();
+        int armRotation = (int) (0.75 * 3895.9);
+
+        AM.setTargetPosition(armRotation);
+        AM.setPower(0.75);
+        while (AM.isBusy()) {
+        }
+        AM.setPower(0);
+
+
+
     }
-
-    /**
-     * Initialize the Vuforia localization engine.
-     */
-    private void initVuforia() {
-        /*
-         * Configure Vuforia by creating a Parameter object, and passing it to the Vuforia engine.
-         */
-        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters();
-
-        parameters.vuforiaLicenseKey = VUFORIA_KEY;
-        parameters.cameraName = hardwareMap.get(WebcamName.class, "Webcam 1");
-
-        //  Instantiate the Vuforia engine
-        vuforia = ClassFactory.getInstance().createVuforia(parameters);
-
-        // Loading trackables is not necessary for the TensorFlow Object Detection engine.
-    }
-
-    /**
-     * Initialize the TensorFlow Object Detection engine.
-     */
-    private void initTfod() {
-        int tfodMonitorViewId = hardwareMap.appContext.getResources().getIdentifier(
-                "tfodMonitorViewId", "id", hardwareMap.appContext.getPackageName());
-        TFObjectDetector.Parameters tfodParameters = new TFObjectDetector.Parameters(tfodMonitorViewId);
-        tfodParameters.minResultConfidence = 0.8f;
-        tfodParameters.isModelTensorFlow2 = true;
-        tfodParameters.inputSize = 320;
-        tfod = ClassFactory.getInstance().createTFObjectDetector(tfodParameters, vuforia);
-        tfod.loadModelFromAsset(TFOD_MODEL_ASSET, LABELS);
-    }
-
 }
