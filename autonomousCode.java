9cd3e332f7d1b8cf2368b6c46d07d693d168ecda
Jacøb <85955074+cuber1515@users.noreply.github.com>
2022-11-21 19:11:26 -0800

    VERY untested EasyOpenCV

--- autonomousCode.java
+++ autonomousCode.java
@@ -1,144 +1,112 @@
-package org.firstinspires.ftc.teamcode;
+package org.openftc.easyopencv;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.Servo;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 
-@Autonomous
-
-public class autonomousCode extends LinearOpMode{
-
-    // start encoders
-    public void startEncoders() {
-        FL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        FR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        BL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        BR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-    }
-
-    // exit encoders
-    public void exitEncoders() {
-        FL.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        FR.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        BL.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        BR.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-    }
-
-    // restart encoders
-    public void resetEncoders() {
-        FL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        FR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        BL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        BR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-    }
-
-    // start encoders arm
-    public void startEncodersA() {
-        AM.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        LAM.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-    }
-
-    // exit encoders arm
-    public void exitEncodersA() {
-        AM.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        LAM.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-    }
-
-    // restart encoders arm
-    public void resetEncodersA() {
-        AM.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        LAM.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-    }
-
-    // drive with encoders
-    public void driveEncoders(double power, int target) {
-        FR.setTargetPosition(target);
-        FL.setTargetPosition(target);
-        BR.setTargetPosition(target);
-        BL.setTargetPosition(target);
-
-        FR.setPower(power);
-        FL.setPower(power);
-        BR.setPower(power);
-        BL.setPower(power);
-
-        FR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        FL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        BR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        BL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-
-        while (FR.isBusy() || FL.isBusy()) {
-        }
-
-        FR.setPower(0);
-        FL.setPower(0);
-        BR.setPower(0);
-        BL.setPower(0);
-    }
-
-    DcMotor FR, FL, BR, BL, AM, LAM;
-    Servo CS;
-
+@Autonomous(name="Auto")
+//@Disabled
+public class autonomousCode extends LinearOpMode {
+    OpenCvWebcam webcam;
     @Override
     public void runOpMode() throws InterruptedException {
-        FR = hardwareMap.dcMotor.get("Front Right");
-        FL = hardwareMap.dcMotor.get("Front Left");
-        BR = hardwareMap.dcMotor.get("Back Right");
-        BL = hardwareMap.dcMotor.get("Back Left");
-        AM = hardwareMap.dcMotor.get("Arm Lift");
-        LAM = hardwareMap.dcMotor.get("Linear Actuator");
-        BR.setDirection((DcMotor.Direction.REVERSE)); // This will reverse the back right wheel (idk why but only this one needs to be reversed)
+        /**
+         * sets up the webcam
+         */
+        WebcamName webcamName = hardwareMap.get(WebcamName.class, "webcam");
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        webcam = OpenCvCameraFactory.getInstance().createWebcam(webcamName, cameraMonitorViewId);
+        /**
+         * calls the pipeline and sets the detector, basically, goober moment
+         */
+        Color1 detecc1 = new Color1(telemetry);
+        Color2 detecc2 = new Color2(telemetry);
+        Color3 detecc3 = new Color3(telemetry);
+        webcam.setPipeline(detecc1);
+        /**
+         * starts the camera working
+         */
+        webcam.openCameraDeviceAsync( new OpenCvCamera.AsyncCameraOpenListener(){
+            @Override
+            public void onOpened() {
+                webcam.startStreaming(1280,720, OpenCvCameraRotation.UPRIGHT);
+            }
 
-        AM.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        LAM.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            @Override
+            public void onError(int errorCode) {
 
-        CS = hardwareMap.servo.get("Claw Servo");
+            }
+        });
 
         waitForStart();
 
-        /*startEncoders();
-        resetEncoders();
-        double circumference = 3.14 * 4;
-        double rotationsNeeded = 24 / circumference;
-        int encoderDrivingTarget = (int) (-rotationsNeeded * 537.7);
-
-        FR.setTargetPosition(encoderDrivingTarget);
-        FL.setTargetPosition(encoderDrivingTarget);
-        BR.setTargetPosition(encoderDrivingTarget);
-        BL.setTargetPosition(encoderDrivingTarget);
-
-        FR.setPower(0.5);
-        FL.setPower(0.5);
-        BR.setPower(0.5);
-        BL.setPower(0.5);
-
-        FR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        FL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        BR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        BL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-
-        while (FR.isBusy() || FL.isBusy() || BR.isBusy() || BL.isBusy()) {
+        /**
+         * make robot do thing
+         */
+        switch (detecc1.getLocation()){
+            case LEFT:
+                /**
+                 * What it does when it's on the left
+                 */
+                telemetry.addData("I see red things", "left");
+                telemetry.update();
+                break;
+            case RIGHT:
+                /**
+                 * What it does when it's on the right
+                 */
+                telemetry.addData("I see red things", "right");
+                telemetry.update();
+                break;
+            case MIDDLE:
+                /**
+                 * What it does when it's in the middle
+                 */
+                telemetry.addData("I see red things", "middle");
+                telemetry.update();
+                break;
+            case CRING:
+                /**
+                 * What it does when it's being weird
+                 */
+                webcam.setPipeline(detecc2);
+                switch (detecc2.getLocation()){
+                    case LEFT:
+                        telemetry.addData("I see green things", "left");
+                        telemetry.update();
+                        break;
+                    case MIDDLE:
+                        telemetry.addData("I see green things", "middle");
+                        telemetry.update();
+                        break;
+                    case RIGHT:
+                        telemetry.addData("I see green things", "right");
+                        telemetry.update();
+                        break;
+                    case CRING:
+                        webcam.setPipeline(detecc3);
+                        switch (detecc3.getLocation()){
+                            case LEFT:
+                                telemetry.addData("I see blue things", "left");
+                                telemetry.update();
+                                break;
+                            case MIDDLE:
+                                telemetry.addData("I see blue things", "middle");
+                                telemetry.update();
+                                break;
+                            case RIGHT:
+                                telemetry.addData("I see blue things", "right");
+                                telemetry.update();
+                                break;
+                            case CRING:
+//ew, gross, women
+                                break;
+                        }
+                        break;
+                }
+                break;
         }
-
-        FR.setPower(0);
-        FL.setPower(0);
-        BR.setPower(0);
-        BL.setPower(0);
-
-        exitEncoders();*/
-
-        FR.setPower(-0.5);
-        FL.setPower(-0.5);
-        BR.setPower(-0.5);
-        BL.setPower(-0.5);
-        sleep(1000);
-        FR.setPower(0);
-        FL.setPower(0);
-        BR.setPower(0);
-        BL.setPower(0);
-        sleep(100);
+        webcam.stopStreaming();
     }
 }
