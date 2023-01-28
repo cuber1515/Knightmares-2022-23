ff0434c03240d62782a74a72f60dfe493ebd988d
Jacøb <85955074+cuber1515@users.noreply.github.com>
2023-01-28 12:05:23 -0800

    Tuned PIDF

--- drive/DriveConstants.java
+++ drive/DriveConstants.java
@@ -32,8 +32,7 @@ public class DriveConstants {
      * from DriveVelocityPIDTuner.
      */
     public static final boolean RUN_USING_ENCODER = true;
-    public static PIDFCoefficients MOTOR_VELO_PID = new PIDFCoefficients(0, 0, 0,
-            getMotorVelocityF(MAX_RPM / 60 * TICKS_PER_REV)); // TODO: tune later
+    public static PIDFCoefficients MOTOR_VELO_PID = new PIDFCoefficients(25, 0, 13.86,13.5);
 
     /*
      * These are physical constants that can be determined from your robot (including the track
--- drive/SampleMecanumDrive.java
+++ drive/SampleMecanumDrive.java
@@ -24,6 +24,7 @@ import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.hardware.PIDFCoefficients;
 import com.qualcomm.robotcore.hardware.VoltageSensor;
 import com.qualcomm.robotcore.hardware.configuration.typecontainers.MotorConfigurationType;
