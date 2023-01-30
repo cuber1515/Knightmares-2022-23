86e096bbbf2d65581ec8e761a14236ff5e5c9419
Jacøb <85955074+cuber1515@users.noreply.github.com>
2023-01-29 20:11:42 -0800

    Update
    Auto: 
      -Has a spline to first step, need to change
      -Added @Config
    TeleOp
      -Added @Config

--- RoadAuto.java
+++ RoadAuto.java
@@ -1,25 +1,39 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.acmerobotics.roadrunner.trajectory.Trajectory;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 
-@Autonomous
+@Config
+@Autonomous(group = "drive", name = "Auto-code")
 public class RoadAuto extends LinearOpMode {
+
+    public static double stX = 36;
+    public static double stY = -64;
+    public static double stHead = 90;
+    public static double endX = 0;
+    public static double endY = -24;
+    public static double endHead = 90;
+
     @Override
-    public void runOpMode() {
+    public void runOpMode() throws InterruptedException {
         SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
 
-        Pose2d startPose = new Pose2d(66, -36, Math.toRadians(90));
+        Pose2d start = new Pose2d(stX, stY, Math.toRadians(stHead));
 
-        drive.setPoseEstimate(startPose);
+        drive.setPoseEstimate(start);
 
-        Trajectory traj1 = drive.trajectoryBuilder(startPose)
-                .splineToConstantHeading(new Vector2d(18, 30), Math.toRadians(0))
+        Trajectory traj1 = drive.trajectoryBuilder(start)
+                .splineToConstantHeading(new Vector2d(endX, endY), Math.toRadians(endHead))
                 .build();
 
         waitForStart();
